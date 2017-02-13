cf63fcbaaf66216b59df9e2ce49596dcb7bed092
John Fox <johnfox4000@gmail.com>
2017-02-12 20:43:08 -0500

    added before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  
   def index
     @posts = Post.all
+  end
+  
+  def show
   end
   
   def new 
@@ -8,28 +13,27 @@ class PostsController < ApplicationController
   end
   
   def create
-    @post = Post.create(post_params)
-    redirect_to posts_path
+    if @post = Post.create(post_params)
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
-  
-  def show
-    @post = Post.find(params[:id])
-  end
-  
+
   def edit
-    @post = Post.find(params[:id])
   end
   
   def update
-    @post = Post.find(params[:id])
-    @post.update(post_params)
-    redirect_to(post_path(@post))
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
   end
   
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
-    redirect_to posts_path
+    redirect_to root_path
   end
   
   
@@ -39,5 +43,9 @@ class PostsController < ApplicationController
   def post_params  
     params.require(:post).permit(:image, :caption)
   end  
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
   
 end
